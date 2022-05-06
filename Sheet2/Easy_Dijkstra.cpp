a0450161c4ff76e2b3d63252d39bb93d44fe5cf1
Omar Osman <45637656+omariosman@users.noreply.github.com>
2022-05-06 22:27:06 +0200

    Update Easy_Dijkstra.cpp

--- Sheet2/Easy_Dijkstra.cpp
+++ Sheet2/Easy_Dijkstra.cpp
@@ -4,80 +4,65 @@ using namespace std;
 
 #define MAX 1061109567
 typedef long long ll;
-ll n;
+
 vector< vector < pair <ll, ll>>> G;
-//ll dis[(int)(1e7 + 6)];
-//ll visited[(int)(1e7+5)];
+ll dis[(int)(1e7 + 6)];
+ll visited[(int)(1e7+5)];
 
 
 
-ll Dij(ll st, ll dest) {
-    vector<ll>dis(n, INT_MAX);
+void Dij(int st) {
 dis[st] = 0;
-priority_queue<pair<ll, ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> PQ;
-PQ.push({0, st});
+priority_queue<pair<ll, ll>> PQ;
+PQ.emplace(0, st);
 while (PQ.size()){
     ll d,u;
-    d = PQ.top().first;
-    u = PQ.top().second;
-    
-    if(u == dest){
-        return d;
-    }
+    tie(d, u) = PQ.top();
     PQ. pop();
-    //d *= -1;
-    if(d > dis[u]) continue;
-    for (pair<ll, ll> &P:G[u]) { 
+    d *= -1;
+    if(dis[u] != d) continue;
+    for (pair<ll, ll> P:G[u]) { 
     ll v, len;
-    v = P.first;
-    len = P.second;
-    if(dis[u]+len < dis[v]){
-        dis[v] = dis[u] + len;
-        PQ.push({dis [v], v});
+    tie(v, len) = P;
+    if(d+len < dis[v]){
+        dis[v] = d + len;
+        PQ.emplace(-dis [v], v);
     }
     }
 }
-return -1;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-cout.tie(NULL);
-   
+   G.resize((int)1e4+5);
 
     int t;
     cin >> t;
     while(t--){
-        ll m;
+        ll n, m;
         cin >>n >> m;
-G.clear();        
-G.resize(n);
-        
-        //memset(visited, 0, sizeof(visited));
-        //memset(dis, 63, sizeof(dis));
 
+
+        
+        memset(visited, 0, sizeof(visited));
+        memset(dis, 63, sizeof(dis));
+for(int i = 0; i <= n; i++)G[i].clear();
         while (m--){
             ll a, b;
             ll cost;
             cin >> a >> b >> cost;    
-            a--;
-            b--;
-            G[a].push_back({b, cost});
-            //G[b].emplace_back(a, cost);
+
+            G[a].emplace_back(b, cost);
+
         }
         ll src, dest;
         cin >> src >> dest;
 
     
     
-        int final_ans = Dij(src-1, dest-1);
-        if (final_ans == -1){
-            cout << "NO\n";
-        } else{
-            cout << final_ans << "\n";
-        }
-        //(dis[dest] >= MAX ? cout << "NO\n" : cout << dis[dest] << "\n");
+        Dij(src);
+        (dis[dest] >= MAX ? cout << "NO\n" : cout << dis[dest] << "\n");
                 
     }
 
